5102dd5e4aba32dc089747b4c43b769601521c5f
cherriesareodd <horizonsbyxryn@gmail.com>
2023-05-09 16:37:38 -0400

    Initial Commit

---
+++ Macie/MacieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacieApp.swift
+//  Macie
+//
+//  Created by Matthew Smith on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MacieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
